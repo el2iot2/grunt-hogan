e58eaa9199ecc915e650a149386b2af33b3de6a1
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2013-02-27 05:30:21 +0000

    applying convention

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,34 @@
-"using strict";
+'using strict';
 module.exports = function(grunt) {
 
-   // Project configuration.
+  // Project configuration.
   grunt.initConfig({
     hogan: {
-        nodejs : {
-            templates : "view/binder/nodejs.hogan",
-            output : "tasks/binder/nodejs.js",
-            binderName: "bootstrap"
-        },
-        "default" : {
-            templates : "view/binder/default.hogan",
-            output : "tasks/binder/default.js",
-            binderName: "bootstrap"
-        },
-        hulk : {
-            templates : "view/binder/hulk.hogan",
-            output : "tasks/binder/hulk.js",
-            binderName: "bootstrap"
-        },
-        revealing : {
-            templates : "view/binder/revealing.hogan",
-            output : "tasks/binder/revealing.js",
-            binderName: "bootstrap"
-        },
-        amd : {
-            templates : "view/binder/amd.hogan",
-            output : "tasks/binder/amd.js",
-            binderName: "bootstrap"
-        }
+      nodejs: {
+        templates: 'view/binder/nodejs.hogan',
+        output: 'tasks/binder/nodejs.js',
+        binderName: 'bootstrap'
+      },
+      'default': {
+        templates: 'view/binder/default.hogan',
+        output: 'tasks/binder/default.js',
+        binderName: 'bootstrap'
+      },
+      hulk: {
+        templates: 'view/binder/hulk.hogan',
+        output: 'tasks/binder/hulk.js',
+        binderName: 'bootstrap'
+      },
+      revealing: {
+        templates: 'view/binder/revealing.hogan',
+        output: 'tasks/binder/revealing.js',
+        binderName: 'bootstrap'
+      },
+      amd: {
+        templates: 'view/binder/amd.hogan',
+        output: 'tasks/binder/amd.js',
+        binderName: 'bootstrap'
+      }
     },
     lint: {
       files: ['grunt.js', 'tasks/**/*.js']
